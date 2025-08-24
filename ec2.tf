d6ee6da07da3effaa5d2e14c8f8e303406f4bcc2
Maira <mairajaws@gmail.com>
2025-08-23 19:35:59 -0700

    updated ec2 with no provider block

--- ec2.tf
+++ ec2.tf
@@ -1,14 +1,10 @@
-    provider "aws" {
-          region = "us-east-1" # Replace with your desired region
-        }
+resource "aws_instance" "my_ec2_instance" {
+    ami           = "ami-00ca32bbc84273381" 
+    instance_type = "t3.micro"
+    
 
-        resource "aws_instance" "my_ec2_instance" {
-          ami           = "ami-0abcdef1234567890" # Replace with a valid AMI ID for your region
-          instance_type = "t2.micro"
-          key_name      = "your-key-pair-name" # Optional: if you want to SSH into the instance
-
-          tags = {
-            Name = "training-temp-vm"
-          }
-        }
+    tags = {
+    Name = "training-temp-vm"
+    }
+}
         
